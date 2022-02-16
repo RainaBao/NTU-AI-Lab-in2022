99eafa9aa91e76c65e54e90f0cd5f3c3413c67b3
RainaBao <49190008+RainaBao@users.noreply.github.com>
2022-02-16 23:12:03 +0800

    Add files via upload
    flask 模型文件

---
+++ app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from keras.models import load_model
+
+
+# In[4]:
+
+
+from flask import request,render_template
+@app.route("/",methods = ["GET","POST"])
+def index():
+    if request.method == "POST":
+        NPTL = request.form.get("NPTL")
+        TLTA = request.form.get("TLTA")
+        WCTA = request.form.get("WCTA")
+        print(NPTL,TLTA,WCTA)
+        model = load_model("BKR")
+        pred = model.predict([[float(NPTL),float(TLTA),float(WCTA)]])
+        print(pred)
+        pred = pred[0][0]
+        s = "The predicted bankrupy score is:"+str(pred)
+        return(render_template("index.html",results = s))
+    else:
+        return(render_template("index.html",results = "2"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
